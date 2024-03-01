e95c1463c055c49a69829bb851556dc2f2493011
Adwall28 <98455674+Adwall28@users.noreply.github.com>
2024-03-01 10:58:11 +0000

    Added Sounds and Improved Cube Movement

--- Script.js
+++ Script.js
@@ -51,6 +51,16 @@ function CubeStartingLocation(){
         });
 }
 
+const PlayerTileArray = ["CubeOne", "CubeTwo", "CubeThree", "CubeFour", 
+                        "CubeFive", "CubeSix", "CubeSeven", "CubeEight",
+                        "CubeNine", "CubeTen", "CubeEleven", "CubeTwelve",
+                        "CubeThirteen", "CubeFourteen", "CubeFifteen", "CubeSixteen"];
+
+
+const MiniTileArray = ["MiniCubeOne", "MiniCubeTwo", "MiniCubeThree", "MiniCubeFour", 
+                        "MiniCubeFive", "MiniCubeSix", "MiniCubeSeven", "MiniCubeEight",
+                        "MiniCubeNine", "MiniCubeTen", "MiniCubeEleven", "MiniCubeTwelve",
+                        "MiniCubeThirteen", "MiniCubeFourteen", "MiniCubeFifteen", "MiniCubeSixteen"];
 
 //Making an Array of all the cube sides
 const DiceSide = ["#TopSide", "#LeftSide", "#RightSide", "#FrontSide", "#BottomSide", "#BackSide"]
@@ -69,20 +79,56 @@ $("" + DiceSide[5] + "").hide();
 //Using the Random Number to show a random side of the cube
 $("" + DiceSide[n] + "").show();
 
-function CubeMovement(){
+var GameEnded = false;
 
-    $(".CubeSide").mousedown(function(){
+
+function CubeMovement(element){
+
+    var HoldingDown;
+    var PickUpSound = new Audio("Sounds/PickUpCube.mp3");
+    var CubeRollSound = new Audio("Sounds/DiceRoll.mp3");
+    var PutDownSound = new Audio("Sounds/PutDownCube.mp3");
+    /*
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    element.mousedown (function(e) {
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // call a function whenever the cursor moves:
+      });
+
+      element.mousemove (function(e) {
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        element.style.top = (element.offsetTop - pos2) + "px";
+        element.style.left = (element.offsetLeft - pos1) + "px";
+      });
+
+      element.mouseup (function(e) {
+        // stop moving when mouse button is released:
+        document.mouseup = null;
+        document.mousemove = null;
+      });
+*/
+
+    element.mousedown(function(){
 
         console.log("Holding down");
-
-        //let HoldingDown = true;
+        PickUpSound.play();
+        HoldingDown = true;
 
         //while (HoldingDown==true){
             
 
             document.addEventListener('keydown', (event) => {
+                CubeRollSound.play();
                 var name = event.key;
-                var code = event.code;
+
                 if (name == 'w'){
                     $("" + DiceSide[n] + "").hide();
                     if (n == 0){
@@ -145,28 +191,36 @@ function CubeMovement(){
                 }
             });
 
-            $(".CubeSide").mousemove(function(event){
-                $(".CubeSide").css({
-                left:  (event.pageX) -60,
-                top:   (event.pageY) -60
-                });
-            });
-
-            $(".CubeSide").mouseup(function(event){
-                $(".CubeSide").css({
-                    left:  (event.pageX) + 1 ,
-                    top:   (event.pageY) + 1 
-                    });
-                console.log("stopped holding down");
-              });
               //break;
         //};   
     });
 
+    element.mousemove(function(event){
+            if (HoldingDown == true){
+                element.css({
+                left:  (event.pageX) -60,
+                top:   (event.pageY) -60
+                });
+            };
+    });
+
+    element.mouseup(function(event){
+        $(".CubeSide").css({
+            left:  (event.pageX) -60,
+            top:   (event.pageY) -60
+            });
+        console.log("stopped holding down");
+        PutDownSound.play();
+        HoldingDown = false;
+      });
+
 };
+
+//var ButtonSound = new Audio("Sounds/ButtonPressed.mp3");
 
 function GameMechanic(){
     $("#ExitButton").click(function(){
+            //ButtonSound.play();
             window.open("", "_self");
             window.close();
     });
@@ -175,6 +229,6 @@ function GameMechanic(){
 $(document).ready(function(){
         timer();
         CubeStartingLocation();
-        setInterval(CubeMovement, 50);
+        setInterval(CubeMovement($(".CubeSide")), 100);
         setInterval(GameMechanic, 10);
 });
